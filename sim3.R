a34a6d267d3da95166a7f0ac213c196b069730c9
xuxieting <88085038+xuxieting@users.noreply.github.com>
2021-08-04 16:06:54 +0800

    Update sim3.R

--- sim3.R
+++ sim3.R
@@ -9,7 +9,7 @@ n = 200 #data size
 p = 10000 #data dim
 r=0.3 #协方差rou 
 r.square<-0.5  ##信噪比
-sim3 = function(n,p,mu,Sigma,r.square,r){
+sim3 = function(n,p,r.square,r){
    mu<- rep(0,p)  #均值
    Sigma <- matrix(NA, ncol=p,nrow=p)
    for(m in 1:p){
@@ -21,7 +21,6 @@ sim3 = function(n,p,mu,Sigma,r.square,r){
    x<- mvrnorm(n=n,mu=mu, Sigma=Sigma)  # 产生服从N（0，Sigmas)的随机数
    beta<-numeric(p)
    beta[1]=3;beta[4]=1.5;beta[7]=2
-   
    sigma.square<-var(x%*%beta)/r.square
    y<-x%*%beta+rnorm(n,0,sqrt(sigma.square))   #拟合回归方程
    #output = screening::screening(x, y, method = 'holp', num.select = n, ebic = TRUE)$screen #这是r 方法
@@ -29,5 +28,5 @@ sim3 = function(n,p,mu,Sigma,r.square,r){
    lag <-sum(c(1,4,7) %in% output)==3
    return(lag)
  }
- result = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,mu,Sigma,r.square,r) ##rerun 100 times
+ result = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r) ##rerun 100 times
  sum(result)/100
