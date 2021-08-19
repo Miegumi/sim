f473ae4133c2ab95e946c5edb8ce542f8e40b730
xuxieting <88085038+xuxieting@users.noreply.github.com>
2021-08-19 10:33:28 +0800

    Update sim3.R

--- sim3.R
+++ sim3.R
@@ -26,10 +26,37 @@ sim3 = function(n,p,r.square,r){
    beta[1]=3;beta[4]=1.5;beta[7]=2
    sigma.square<-var(x%*%beta)/r.square
    y<-x%*%beta+rnorm(n,0,sqrt(sigma.square))   #拟合回归方程
-   #output = screening::screening(x, y, method = 'holp', num.select = n, ebic = TRUE)$screen #这是r 方法
-   output = myscreening::my_screening(x, y, method = 'holp', num.select = n, ebic = TRUE)$screen #这是C++ 方法
-   lag <-sum(c(1,4,7) %in% output)==3
-   return(lag)
- }
- result = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r) ##rerun 100 times
- sum(result)/100
+   ##以下为r方法
+   #output_holp = screening::screening(x, y, method = 'holp', num.select = n, ebic = TRUE)$screen #这是r 方法
+   #output_sis = screening::screening(x, y, method = 'sis', num.select = n, ebic = TRUE)$screen  #这是r 方法
+   #output_rrcs = screening::screening(x, y, method = 'rrcs', num.select = n, ebic = TRUE)$screen  #这是r 方法
+   #output_isis = SIS::SIS(x,y,family='gaussian', tune='bic',iter=TRUE)$ix    #这是r 方法
+   #output_forward = screening::screening(x, y, method = 'forward', num.select = n, ebic = TRUE)$screen  #这是r 方法
+
+   ##以下为c++方法
+   output_holp = myscreening::my_screening(x, y, method = 'holp', num.select = n, ebic = TRUE)$screen #这是C++ 方法
+   output_sis = myscreening::my_screening(x, y, method = 'sis', num.select = n, ebic = TRUE)$screen #这是C++ 方法
+   output_rrcs = myscreening::my_screening(x, y, method = 'rrcs', num.select = n, ebic = TRUE)$screen #这是C++ 方法
+   output_isis = SIS::SIS(x,y,family='gaussian', tune='bic',iter=TRUE)$ix #这是C++ 方法
+   output_forward = myscreening::my_screening(x, y, method = 'forward', num.select = n, ebic = TRUE)$screen #这是C++ 方法
+   
+lag_holp<-sum(c(1,4,7) %in% output_holp)==3
+lag_sis<-sum(c(1,4,7) %in% output_sis)==3
+lag_rrcs<-sum(c(1,4,7) %in% output_rrcs)==3
+lag_isis<-sum(c(1,4,7) %in% output_isis)==3
+lag_forward<-sum(c(1,4,7) %in% output_forward)==3
+
+return(list(lag_holp,lag_sis,lag_rrcs,lag_isis,lag_forward))
+}
+result_holp = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r)$lag_holp ##rerun 100 times
+result_sis = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r)$lag_sis
+result_rrcs = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r)$lag_rrcs
+result_isis = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r)$lag_isis
+result_forward = foreach(i=1:100, .combine = "rbind") %do% sim3(n,p,r.square,r)$lag_forward
+
+sum(result_holp)/100
+sum(result_sis)/100
+sum(result_rrcs)/100
+sum(result_isis)/100
+sum(result_forward)/100
+
