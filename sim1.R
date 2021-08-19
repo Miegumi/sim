0d1c5c11da5099ef05fcc064a5f7f4ce396b1569
xuxieting <88085038+xuxieting@users.noreply.github.com>
2021-08-19 23:07:35 +0800

    Update sim1.R

--- sim1.R
+++ sim1.R
@@ -17,8 +17,8 @@ sim1 = function(n,p,p1,r.square){
  for(i in 1:p1){
    beta[i]<-(-1)^u[i]*(abs(rnorm(1))+4*log(n)/sqrt(n))   #生成beta
   }
- for (i in 1:n){
-   x[i,]=rnorm(p,0,1)
+ for (j in 1:p){
+   x[,j]=rnorm(p,0,1)
   }
  sigma.square<-var(x%*%beta)/r.square
  y <- x%*%beta+rnorm(n,0,sqrt(sigma.square))   #拟合回归方程
